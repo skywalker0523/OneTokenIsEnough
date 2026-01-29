9ebcec75ff022e652cb89a64307c910d4c279a51
ssskywalker <15188736811@163.com>
2026-01-29 19:57:40 +0800

    Update eval_mdm_gate.sh

--- eval_mdm_gate.sh
+++ eval_mdm_gate.sh
@@ -1,15 +1,15 @@
 
 
-python evaluate_diff.py --tasks lambada_standard --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='chain_rule',greddy=True,cfg=1.1
+python evaluate_diff_gate.py --tasks lambada_standard --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='chain_rule',greddy=True,cfg=1.1
 
-python evaluate_diff.py --tasks openbookqa,boolq --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='chain_rule',greddy=False,cfg=1.6
+python evaluate_diff_gate.py --tasks openbookqa,boolq --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='chain_rule',greddy=False,cfg=1.6
 
-python evaluate_diff.py --tasks piqa --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='chain_rule',greddy=False,cfg=0.6
+python evaluate_diff_gate.py --tasks piqa --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='chain_rule',greddy=False,cfg=0.6
 
-python evaluate_diff.py --tasks arc_easy --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.6
+python evaluate_diff_gate.py --tasks arc_easy --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.6
 
-python evaluate_diff.py --tasks social_iqa --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.6
+python evaluate_diff_gate.py --tasks social_iqa --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.6
 
-python evaluate_diff.py --tasks race --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.7
+python evaluate_diff_gate.py --tasks race --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.7
 
-python evaluate_diff.py --tasks hellaswag --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.7
+python evaluate_diff_gate.py --tasks hellaswag --model mdlm --batch_size 16 --model_args model_name=472,ckpt_path='your_model_save_path',nll_type='mc',greddy=False,cfg=0.7
